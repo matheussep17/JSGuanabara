d10e9fdd7762f315774e30f86ed92176f6450ccd
matheussep17 <matheussep17@gmail.com>
2019-07-08 12:04:39 -0300

    Switch e date

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = -22;
+if (idade <16 && idade >= 0){
+    console.log('Não vota');   
+} else if (idade >= 16 && idade <18) {
+    console.log('Voto opcional');
+ } else if (idade> 18){
+    console.log('Voto obrigatório');
+ } else { 
+     console.log('idade inválida'); 
+}
