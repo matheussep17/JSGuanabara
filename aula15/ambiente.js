f67257fc68f14b86c24a5578c3d28ef219529c33
matheussep17 <matheussep17@gmail.com>
2019-07-11 09:23:12 -0300

    for

---
+++ aula15/ambiente.js
@@ -0,0 +1,12 @@
+// var c= 1;
+
+// while(c <=6){
+//     console.log(`Conte: ${c}`);
+//     c++;
+// 
+console.log('Vai começar');
+
+for (c=1; c<=6; c++){
+    console.log(`O numero atual é: ${c}`);
+}
+console.log('Fim');
