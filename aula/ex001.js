243465b1a848db77e5a3e71f1bf4d1da2160df7f
matheussep17 <matheussep17@gmail.com>
2019-07-14 12:21:12 -0300

    Vetores

---
+++ aula/ex001.js
@@ -0,0 +1,6 @@
+let num = [5,8,3,2,56,2342];
+num.sort();
+console.log(num.indexOf(2342));
+
+console.log(`Os valores do vetor são ${num.sort()}`);
+
---
+++ aula/vetor.js
@@ -0,0 +1,7 @@
+let valores = [0,1,7,4,9,2];
+
+for (let pos =0; pos < valores.length; pos++ ){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`);
+    console.log(num.indexOf(7));
+}
+
